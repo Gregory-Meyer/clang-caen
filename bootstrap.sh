647fd38f6059ae3b31353a44c8d2cece900e442c
Gregory Meyer <gregjm@umich.edu>
2019-03-09 21:42:14 -0500

    little qol changes, like deleting ninja

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ yum upgrade -y
 yum install -y ninja-build wget
 yum groupinstall -y "Development Tools"
 
-wget -q https://github.com/Kitware/CMake/releases/download/v3.13.4/cmake-3.13.4-Linux-x86_64.tar.gz
+wget https://github.com/Kitware/CMake/releases/download/v3.13.4/cmake-3.13.4-Linux-x86_64.tar.gz
 tar xf cmake-3.13.4-Linux-x86_64.tar.gz
 rm cmake-3.13.4-Linux-x86_64.tar.gz
 cp -a cmake-3.13.4-Linux-x86_64/. /usr/local/
@@ -12,10 +12,7 @@ rm -rf cmake-3.13.4-Linux-x86_64
 
 git clone git://github.com/ninja-build/ninja.git
 cd ninja
+git checkout release
 python configure.py --bootstrap
 cp ninja /usr/local/bin/ninja
-
-mkdir -p /usr/local/src
-git clone https://github.com/llvm/llvm-project.git
-cd llvm-project
-git checkout llvmorg-7.0.1
+rm -rf ninja
